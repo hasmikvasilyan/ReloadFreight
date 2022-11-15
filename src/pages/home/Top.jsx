c0a2b7602b80b8e33e24c6d996537bd00f2469cc
HasmikOsensus <hasmik@osensus.com>
2022-11-15 14:26:47 +0400

    front page responsive page

--- src/pages/home/Top.jsx
+++ src/pages/home/Top.jsx
@@ -1,34 +1,35 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
 import logo from "../../assets/img/svg/logo.svg";
-import Navigation from '../../components/Navigation';
-
+import Navigation from "../../components/Navigation";
+import { ReactComponent as WatchIcon } from "../../assets/img/svg/watchIcon.svg";
 
 function Top() {
-    
   return (
     <section className="top">
-        <div className="container">
-            <header>                
-                <Link to='/' className='logo'><img src={logo} alt="reload freight"/></Link>
-                <Navigation/>
-                
-            </header>
-            <div className="sloganWrapper">
-                <h1 className="slogan">
-                    <span>The one & only</span> Landstar agency in Armenia
-                </h1>
-                <div className="btnGroup">
-                    <Link to='/' className='btn btnFilled'>Join the circle</Link>
-                    <Link to='/' className='btn btnOutline'>Watch the video</Link>
-                </div>
-            </div>
-    
+      <div className="container">
+        <header>
+          <Link to="/" className="logo">
+            <img src={logo} alt="reload freight" />
+          </Link>
+          <Navigation />
+        </header>
+        <div className="sloganWrapper">
+          <h1 className="slogan">
+            <span>The one & only</span> Landstar agency in Armenia
+          </h1>
+          <div className="btnGroup">
+            <Link to="/" className="btn btnFilled">
+              Join the circle
+            </Link>
+            <Link to="/" className="btn btnOutline">
+              <WatchIcon />
+              Watch the video
+            </Link>
+          </div>
         </div>
+      </div>
     </section>
-    
-
-  )
+  );
 }
 
-export default Top
+export default Top;
